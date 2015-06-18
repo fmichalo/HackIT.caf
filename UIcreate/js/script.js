1354846e7367dbfb39b97b8070a1150f5038942d
fmichalo <fmichalo@cisco.com>
2015-06-19 00:40:00 +0200

    add and move module ok

--- UIcreate/js/script.js
+++ UIcreate/js/script.js
@@ -3,34 +3,48 @@ function importModule(moduleName){
 
 	$.ajax({
 		'url':'../modules/'+ moduleName +'/'+moduleName +'.html',
-		'success':function(data){$('#content').append('<div class="well span8 tile">'+data+'</div>')}
+		'success':function(data){$('#content').append('<div class="well span7 tile">'+data+'</div>')}
 	});
 
 }
-/*
-function jsonToList(jsonname){
+
+function jsonToList(){
+
+	
 
 	$.getJSON( "../modules/listModules.json", function( data ) {
+		
 	  var items = [];
 	  $.each( data, function( key, val ) {
-	    items.push( "<li id='" + key + "'>" + val + "</li>" );
+	    items.push( "<li id='module" + key + "'>" + val + " <button class='linkAdd' id='"+ val+"'>Add</button></li>" );
 	  });
-	 
+
+	
+
 	  $( "<ul/>", {
-	    "class": "my-new-list",
+	    "class": "listModules",
 	    html: items.join( "" )
 	  }).appendTo( "#moduleSelect" );
+
+	  $( ".linkAdd" ).click(function() {
+	  	var idmodule = this.id;
+		 importModule(idmodule);
+		});
+	
 	});
 
+
+
 }
-*/
+
+
 $(function () {
-    $(".grid").sortable({
+    $("#content").sortable({
         tolerance: 'pointer',
         revert: 'invalid',
-        placeholder: 'span2 well placeholder tile',
+        placeholder: 'span7 well placeholder tile',
         forceHelperSize: true,
-        cancel: "p"
+        cancel: "p, h2"
     });
 });
 
