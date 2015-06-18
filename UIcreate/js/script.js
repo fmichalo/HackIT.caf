1d888028a979001742827a61aa7558834e4720ac
fmichalo <fmichalo@cisco.com>
2015-06-18 18:53:34 +0200

    HTML squelette

--- UIcreate/js/script.js
+++ UIcreate/js/script.js
@@ -6,12 +6,4 @@ function importModule(moduleName){
 		'success':function(data){$('#content').append(data)}
 	});
 
-	// var xhr= new XMLHttpRequest();
-	// xhr.open('GET', '../../modules/'+ moduleName +'/'+moduleName +'.html', true);
-	// xhr.onreadystatechange= function() {
-	//     if (this.readyState!==4) return;
-	//     if (this.status!==200) return; // or whatever error handling you want
-	//     document.getElementById('content').innerHTML= this.responseText;
-	// };
-	// xhr.send();
 }
