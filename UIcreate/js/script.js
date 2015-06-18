c2ccd62d68b9c86b13afc1b6e34a5929e7c73486
fmichalo <fmichalo@cisco.com>
2015-06-18 18:44:58 +0200

    Add module working

--- UIcreate/js/script.js
+++ UIcreate/js/script.js
@@ -1,12 +1,17 @@
 
 function importModule(moduleName){
 
-	var xhr= new XMLHttpRequest();
-	xhr.open('GET', '../modules/'+ moduleName +'.html', true);
-	xhr.onreadystatechange= function() {
-	    if (this.readyState!==4) return;
-	    if (this.status!==200) return; // or whatever error handling you want
-	    document.getElementById('content').innerHTML= this.responseText;
-	};
-	xhr.send();
+	$.ajax({
+		'url':'../modules/'+ moduleName +'/'+moduleName +'.html',
+		'success':function(data){$('#content').append(data)}
+	});
+
+	// var xhr= new XMLHttpRequest();
+	// xhr.open('GET', '../../modules/'+ moduleName +'/'+moduleName +'.html', true);
+	// xhr.onreadystatechange= function() {
+	//     if (this.readyState!==4) return;
+	//     if (this.status!==200) return; // or whatever error handling you want
+	//     document.getElementById('content').innerHTML= this.responseText;
+	// };
+	// xhr.send();
 }
