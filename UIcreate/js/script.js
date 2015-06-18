b5059ee5ace376127c0a228b80a44faaf7d50da1
unknown <xchemla@XCHEMLA-WS.cisco.com>
2015-06-18 19:51:37 +0200

    add list of modules

--- UIcreate/js/script.js
+++ UIcreate/js/script.js
@@ -7,3 +7,19 @@ function importModule(moduleName){
 	});
 
 }
+
+function jsonToList(jsonname){
+
+	$.getJSON( "../modules/listModules.json", function( data ) {
+	  var items = [];
+	  $.each( data, function( key, val ) {
+	    items.push( "<li id='" + key + "'>" + val + "</li>" );
+	  });
+	 
+	  $( "<ul/>", {
+	    "class": "my-new-list",
+	    html: items.join( "" )
+	  }).appendTo( "#moduleSelect" );
+	});
+
+}
