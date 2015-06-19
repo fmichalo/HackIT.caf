ad714129335750ed06c816d2fefa1e63faa1385f
fmichalo <fmichalo@cisco.com>
2015-06-19 06:27:07 +0200

    add module and correct bug
    add module image and text
    correct responsive bug

--- modules/imageAndText/script.js
+++ modules/imageAndText/script.js
@@ -1,9 +1,12 @@
-$( document ).ready(function() {
+function eventimg(){
+	$( document ).ready(function() {
+	$( ".editImage" ).unbind( "click" );
 	$( ".editImage" ).click(function() {
 		var newURL= prompt('New image url','http://url.com/image.png');
 		$(this).prev().attr("src",newURL);
 	});
 
-});
+});};
 
+eventimg();
 var imgscript=true;
