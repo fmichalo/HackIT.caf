93ec035225161b91713386859bacbf385e9ac491
fmichalo <fmichalo@cisco.com>
2015-06-19 03:43:43 +0200

    Big push
    add title
    add subscribe
    add info form
    add css

--- UIcreate/js/script.js
+++ UIcreate/js/script.js
@@ -3,7 +3,11 @@ function importModule(moduleName){
 
 	$.ajax({
 		'url':'../modules/'+ moduleName +'/'+moduleName +'.html',
-		'success':function(data){$('#content').append('<div class="well span7 tile">'+data+'</div>')}
+		'success':function(data){$('#content').append('<div class="well span7 tile">'+data+'<button class="deleteButton">delete</button></div>')
+		$( ".deleteButton" ).click(function() {
+			$(this).parent().remove();
+		});
+	}
 	});
 
 }
@@ -44,7 +48,41 @@ $(function () {
         revert: 'invalid',
         placeholder: 'span7 well placeholder tile',
         forceHelperSize: true,
-        cancel: "p, h2"
+        cancel: "p, h2, form, button"
     });
+});
+
+function addAttendee(json) {
+
+	$.ajax({
+	type: "POST",
+        //the url where you want to sent the userName and password to
+        url: 'addAttendee',
+        dataType: 'json',
+        async: false,
+        //json object to sent to the authentication url
+        data: '{ "attendee" : '+ json+ '}',
+        success: function () {
+
+        alert("Thanks!"); 
+        }}
+    );
+
+	// $.ajax({
+	// 	type : "POST",
+	// 	url : "addAttendee?json=" + json,
+	// 	success : function(response) {
+	// 	$("#subscribe").html("Inscription Done ! ");
+	// }
+	// });
+}
+
+$( document ).ready(function() {
+	$( "#imgEdit" ).click(function() {
+		var newURL= prompt('New banner url','http://url.com/image.png');
+		console.log(newURL);
+		$("#BannerImg").attr("src",newURL);
+	});
+
 });
 
---
+++ modules/subscribe/script.js
@@ -0,0 +1,26 @@
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+$(function() {
+    $('form').submit(function() {
+        //addAttendee()
+        var json = JSON.stringify($('form').serializeObject());
+        //var json2 = encodeURIComponent(JSON.stringify(json));
+        addAttendee(json);
+        return false;
+    });
+});
